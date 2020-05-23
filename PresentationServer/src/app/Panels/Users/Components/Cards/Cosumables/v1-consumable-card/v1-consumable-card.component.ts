71b642d9ab6f8e75e8f0b6948d2b5d227ce72c8c
shivamchapslee <shivamchapslee@gmail.com>
2020-05-23 19:20:40 +0530

    worked on card before using flex

--- PresentationServer/src/app/Panels/Users/Components/Cards/Cosumables/v1-consumable-card/v1-consumable-card.component.ts
+++ PresentationServer/src/app/Panels/Users/Components/Cards/Cosumables/v1-consumable-card/v1-consumable-card.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { GetConsumablesDetailsService } from 'src/app/Panels/Users/Services/MasterDataManagement/Consumables/Get_Consumables_Details_Service/get-consumables-details.service';
+import { V1ReceivedConsumables } from 'src/app/Panels/Users/Models/MasterDataManagement/Consumables/Received_Consumables/v1-received-consumables.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-v1-consumable-card',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./v1-consumable-card.component.scss']
 })
 export class V1ConsumableCardComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  receivedConsumablesDetails: V1ReceivedConsumables[];
+  public visible: boolean = false;
+  obs: Observable<any>;
+  dataSource: MatTableDataSource<V1ReceivedConsumables>
+
+  constructor(public getConsumableDetailsService: GetConsumablesDetailsService, private changeDetectorRef: ChangeDetectorRef ) { }
 
   ngOnInit(): void {
+  }
+
+  getConsumables()
+  {
+    this.receivedConsumablesDetails = this.getConsumableDetailsService.consumableDetails; 
+    //console.log("card data", this.receivedConsumablesDetails);
+    this.visible = true;
+    this.dataSource = new MatTableDataSource(this.receivedConsumablesDetails);
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.obs = this.dataSource.connect();
   }
 
 }
