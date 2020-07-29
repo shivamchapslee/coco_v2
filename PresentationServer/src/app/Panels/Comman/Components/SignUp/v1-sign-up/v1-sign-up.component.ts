08b34804965c687d01cd7a5cde67fa156471e206
shivamchapslee <shivamchapslee@gmail.com>
2020-07-29 21:52:06 +0530

    added functionality without validation

--- PresentationServer/src/app/Panels/Comman/Components/SignUp/v1-sign-up/v1-sign-up.component.ts
+++ PresentationServer/src/app/Panels/Comman/Components/SignUp/v1-sign-up/v1-sign-up.component.ts
@@ -12,7 +12,7 @@ import { V1SignUpSubmittion } from '../../../Models/SignUp_Model/v1-sign-up-subm
 export class V1SignUpComponent implements OnInit {
 
   public valid: boolean =  false;
-  public userData: V1SignUpSubmittion = new V1SignUpSubmittion(); 
+  public userData: V1SignUpSubmittion = new V1SignUpSubmittion();   
 
   public insertUserDetails = new FormGroup({
     firstName       :       new FormControl('',Validators.required),
@@ -22,7 +22,8 @@ export class V1SignUpComponent implements OnInit {
     gender          :       new FormControl('',Validators.required),
     userName        :       new FormControl('',Validators.required),
     Password        :       new FormControl('',Validators.required),
-    firstLoginFlag  :       new FormControl('',Validators.required)
+    firstLoginFlag  :       new FormControl('',Validators.required),
+    PasswordCheck   :       new FormControl('',Validators.required)
   })
 
   constructor(private _notification: NotificationsService, public signUpResponse:V1SignUpServiceService) { }
@@ -30,55 +31,55 @@ export class V1SignUpComponent implements OnInit {
   ngOnInit(): void {    
   }
 
-  get f() { return this.insertUserDetails.controls; }
+  get f() { return this.insertUserDetails.controls; } 
 
   onSubmitClick()
   {
-    let userDetails = 
+    console.log("outer");
+    console.log(this.insertUserDetails.valid);
+    if (this.insertUserDetails.valid) 
     {
-      First_Name: "Sunakshi",
-      Last_Name: "Sharma",
-      EMAIL_Address:"afdfsfd@gmail.com",
-      Phone_Number:"1234567898",
-      Gender: "F",
-      User_Name: "Sona",
-      Password: "sameasabove",
-      First_Login_Flag: false
-    }
+      console.log("called");
 
-    this.userData.First_Name        =     this.insertUserDetails.value.firstName;
-    this.userData.Last_Name         =     this.insertUserDetails.value.lastName;
-    this.userData.EMAIL_Address     =     this.insertUserDetails.value.email;
-    this.userData.Phone_Number      =     this.insertUserDetails.value.phone;
-    this.userData.Gender            =     this.insertUserDetails.value.gender;
-    this.userData.User_Name         =     this.insertUserDetails.value.userName;
-    this.userData.Password          =     this.insertUserDetails.value.Password;
-    // this.userData.First_Login_Flag  =     this.insertUserDetails.value.firstLoginFlag;
+      this.userData.First_Name        =     this.insertUserDetails.value.firstName;
+      this.userData.Last_Name         =     this.insertUserDetails.value.lastName;
+      this.userData.EMAIL_Address     =     this.insertUserDetails.value.email;
+      this.userData.Phone_Number      =     this.insertUserDetails.value.phone;
+      this.userData.Gender            =     this.insertUserDetails.value.gender;
+      this.userData.User_Name         =     this.insertUserDetails.value.userName;
+      this.userData.Password          =     this.insertUserDetails.value.Password;      
+      // this.userData.First_Login_Flag  =     this.insertUserDetails.value.firstLoginFlag;
+      
+      console.log("userDetails", this.userData);
+        return;   
 
-    
-    console.log("userDetails", this.userData);
-    //  return;   
-
-    this.signUpResponse.userSubmittion(this.userData)
-    .subscribe(
-      (response) => 
-      {
-        this._notification.responseHandler(response); 
-        if(response["response"] == 1)
+      this.signUpResponse.userSubmittion(this.userData)
+      .subscribe(
+        (response) => 
         {
-          console.log("this is it",response);
-          this.insertUserDetails.reset();
-          //this._notification.responseHandler(response["data"][0]["RESPONSE"]); 
+          this._notification.responseHandler(response); 
+          if(response["response"] == 1)
+          {
+            console.log("this is it",response);
+            this.insertUserDetails.reset();
+            //this._notification.responseHandler(response["data"][0]["RESPONSE"]); 
 
-          // this.authlogin =  response["data"];               
-          // if(response["data"][0][0]["RESPONSE"] == 1) 
-          // {
-          //   console.log(response); 
-          //   this.router.navigateByUrl("#");
-          // }
+            // this.authlogin =  response["data"];               
+            // if(response["data"][0][0]["RESPONSE"] == 1) 
+            // {
+            //   console.log(response); 
+            //   this.router.navigateByUrl("#");
+            // }
+          }
         }
-      }
-    )
+      )
+    }
+    else
+    {
+      this._notification.info("Please enter all required fields")
+       this.valid =  true; 
+    }
   }
+ 
 
 }
